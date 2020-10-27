bb785ad1249b9bc9778921271e5da6b48be72db9
Rodolfo <rod.vgo@gmail.com>
2020-10-27 18:32:01 -0300

    mudanças

---
+++ introduction-web_development/bloco_4/4.3/algoritimo1.js
@@ -0,0 +1,58 @@
+
+/*  Exercicio 1
+
+    1- Criar um array
+    2- Colocar valores numericos aleatorios nesse array
+    3- Criar um loop usando for que percorra o array
+    4- Imprima todos os valores utilizando a função console.log() 
+
+    Exercicio 2
+
+    1- Usar o array do exercício anterior
+    2- Criar um variavel com valor 0
+    3- Criar um loop que percorra o array
+    4- Atribuir cada valor a variavel criada a cada loop
+    5- Imprimir o valor final da variável;
+
+    Exercicio 3
+
+    1- Usar o array anterior
+    2- Criar uma variavel com valor 0
+    3- Criar um loop que percorra todo o array
+    4- Somar cada valor do array à variavel criada
+    5- Criar outra variavel com valor 0
+    6- Dentro da segunda variavel criada, dividir o valor da primeira variavel
+       pelo numero de elementos do array.
+    7- Imprimir o valor final da segunda variavel utilizando função console.log()
+
+    Exercicio 4
+
+    1- Usar o array anterior
+    2- Criar uma variavel com valor 0
+    3- Criar um loop que percorra todo o array
+    4- Somar cada valor do array à variavel criada
+    5- Criar outra variavel com valor 0
+    6- Dentro da segunda variavel criada, dividir o valor da primeira variavel
+       pelo numero de elementos do array.
+    7- Utilize função if e imprima "valor maior que 20" caso isso seja verdade,
+       caso nao seja, imprima: "valor menor ou igual a 20"
+
+    Exercicio 5
+
+    1- Usar o array anterior
+    2- Criar uma variavel com valor 0
+    3- Criar loop que percorra toda a array
+    4- Substituir o valor contido na variavel pelo valor da array caso
+       caso esse seja maior que o valor atual da variavel
+    5- Imprima o valorfinal da variavel
+
+    Exercicio 6
+
+    1- Usar array anterior
+    2- Criar uma variavel com valor 0
+    3- Criar um loop que percorra todo array
+    4- Utilizando if caso o elemento do array seja impar, some "1" a variavel criada
+    5- Imprima o valor contido na variavel caso esse valor seja maior que "0", caso
+    não tenha nenhum imprima "nenhum valor impar encontrado"
+
+    
