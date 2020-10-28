8bef298e4c80d35168dfa74edb2c1a63474d2b2a
Rodolfo <rod.vgo@gmail.com>
2020-10-28 13:08:34 -0300

    exercicio mega sena

--- introduction-web_development/bloco_4/4.2/megasena.js
+++ introduction-web_development/bloco_4/4.2/megasena.js
@@ -10,14 +10,17 @@ let sixthNumber = Math.ceil(Math.random() * 60);
 megaSenaNumbers = [fifithNumber, secondNumber, thirdNumber, fourthNumber, fifithNumber, sixthNumber];
 
 let turma7Play = [7, 16, 28, 31, 37, 55];
+let numberOfHits = 0;
 
 for (let i = 0; i < turma7Play.length; i += 1) {
-    let numeroJogado = turma7Play[i];
+    let playedNumber = turma7Play[i];
     for (let j = 0; j < megaSenaNumbers.length; j += 1) {
-        let 
-        if (megaSenaNumbers[j] == numeroJogado) {
-
+        let numberMegaSena = megaSenaNumbers[j];
+        if(playedNumber == numberMegaSena) {
+            numberOfHits += 1;
         }
     }
 }
 
+console.log(megaSenaNumbers);
+console.log(numberOfHits);
