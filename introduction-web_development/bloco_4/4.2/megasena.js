56a055cb15a8378a809edb1cd2bd8dfffb7be0a3
Rodolfo <rod.vgo@gmail.com>
2020-10-28 14:07:59 -0300

    mega sena

--- introduction-web_development/bloco_4/4.2/megasena.js
+++ introduction-web_development/bloco_4/4.2/megasena.js
@@ -9,18 +9,27 @@ let sixthNumber = Math.ceil(Math.random() * 60);
 
 megaSenaNumbers = [fifithNumber, secondNumber, thirdNumber, fourthNumber, fifithNumber, sixthNumber];
 
-let turma7Play = [20, 16, 28, 31, 37, 55];
+let play1 = [20, 16, 28, 31, 37, 55];
+let play2 = [45, 27, 92, 19, 31, 12];
+let play3 = [38, 16, 89, 74, 57, 82];
+
+let plays = [play1, play2, play3];
+
 let numberOfHits = 0;
 
-for (let i = 0; i < turma7Play.length; i += 1) {
-    let playedNumber = turma7Play[i];
-    for (let j = 0; j < megaSenaNumbers.length; j += 1) {
-        let numberMegaSena = megaSenaNumbers[j];
-        if(playedNumber == numberMegaSena) {
-            numberOfHits += 1;
+
+for (let index = 0; index < plays.length; index += 1) {
+    let playCurrent = plays[index];
+    for (let i = 0; i < playCurrent.length; i += 1) {
+        let playedNumber = playCurrent[i];
+        for (let j = 0; j < megaSenaNumbers.length; j += 1) {
+            let numberMegaSena = megaSenaNumbers[j];
+            if(playedNumber == numberMegaSena) {
+                numberOfHits += 1;
+            }
         }
     }
+    console.log(`Jogo a ser conferido: ${playCurrent}`);
+    console.log(`Números sorteados: ${megaSenaNumbers}`);
+    console.log(`Acertos do nosso jogo: ${numberOfHits}`);
 }
-
-console.log(`Números sorteados: ${megaSenaNumbers}`);
-console.log(`Acertos do nosso jogo: ${numberOfHits}`);
