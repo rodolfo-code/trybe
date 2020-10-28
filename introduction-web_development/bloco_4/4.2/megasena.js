a97f86b0f13b776cc8ac3dff510a55e6982d2fba
Rodolfo <rod.vgo@gmail.com>
2020-10-28 11:47:12 -0300

    estudo dos exercicios

--- introduction-web_development/bloco_4/4.2/megasena.js
+++ introduction-web_development/bloco_4/4.2/megasena.js
@@ -1,8 +1,5 @@
 let megaSenaNumbers = [];
 
-
-
-
 let firstNumber = Math.ceil(Math.random() * 60);
 let secondNumber = Math.ceil(Math.random() * 60);
 let thirdNumber = Math.ceil(Math.random() * 60);
@@ -12,4 +9,15 @@ let sixthNumber = Math.ceil(Math.random() * 60);
 
 megaSenaNumbers = [fifithNumber, secondNumber, thirdNumber, fourthNumber, fifithNumber, sixthNumber];
 
-let 
+let turma7Play = [7, 16, 28, 31, 37, 55];
+
+for (let i = 0; i < turma7Play.length; i += 1) {
+    let numeroJogado = turma7Play[i];
+    for (let j = 0; j < megaSenaNumbers.length; j += 1) {
+        let 
+        if (megaSenaNumbers[j] == numeroJogado) {
+
+        }
+    }
+}
+
