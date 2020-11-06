d16774630003051bcc8f1a706915ab72db20171c
rodolfo_oliveira <rod.vgo@gmail.com>
2020-11-05 23:48:19 -0300

    exercicio 1

--- introduction-web_development/bloco_4/4.3/exercise_1.js
+++ introduction-web_development/bloco_4/4.3/exercise_1.js
@@ -1,9 +1,9 @@
 let n = 5;
 let symbol = "*";
-let inputLine = "";
+let inputLine = "" ;
 
 for (let i = 0; i < n; i += 1) {
-    inputLine = inputLine +  symbol;
+    inputLine += symbol;
 }
 
 for (let i = 0; i < n; i += 1) {
