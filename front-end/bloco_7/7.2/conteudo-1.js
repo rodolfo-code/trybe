ff0ff7504c63919e9839fbc30d3e900d43f810f9
rodolfo_oliveira <rod.vgo@gmail.com>
2020-11-24 15:43:21 -0300

    conteudo sobre objeto 7.2

---
+++ front-end/bloco_7/7.2/conteudo-1.js
@@ -0,0 +1,15 @@
+const customer = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+console.log(customer);
+
+customer.lastName = 'Oliveira';
+
+console.log(customer)
+
+customer['firstName'] = 'Rodolfo';
+
+console.log(customer);
