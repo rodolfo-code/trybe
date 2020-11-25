a5caaf9e8493ee9307ffc93a93df8613b4d5e867
rodolfo_oliveira <rod.vgo@gmail.com>
2020-11-24 23:24:51 -0300

    exercicio 2 parte 1

--- front-end/bloco_7/7.2/exercicio 1 parte 2.js
+++ front-end/bloco_7/7.2/exercicio 1 parte 2.js
@@ -1,63 +1,33 @@
-const order = {
-  name: 'Rafael Andrade',
-  phoneNumber: '11-98763-1416',
-  address: {
-    street: 'Rua das Flores',
-    number: '389',
-    apartment: '701',
-  },
-  order: {
-    pizza: {
-      margherita: {
-        amount: 1,
-        price: 25,
-      },
-      pepperoni: {
-        amount: 1,
-        price: 20,
-      }
-    },
-    drinks: {
-      coke: {
-        type: 'Coca-Cola Zero',
-        price: 10,
-        amount: 1,
-      }
-    },
-    delivery: {
-      deliveryPerson: 'Ana Silveira',
-      price: 5,
-    }
-  },
-  payment: {
-    total: 60,
-  },
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
 };
 
-const customerInfo = (order) => {
-  // Adicione abaixo as informações necessárias.
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
 
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+
+// lesson2['turno'] = 'manhã';
+// console.log(lesson2)
+
+function addShift (obj, key, value) {
+  let objetoDestino = obj;
+  const chave = key;
+  const valor = value;
+  objetoDestino[chave] = valor
+  return objetoDestino;
 }
 
-customerInfo(order);
-
-const orderModifier = (order) => {
-  const pizzas = ['muzzarella', 'calabresa'];
-  order.order.pizza = {};
-  const buyer = order.name = 'Luiz Silva';
-  order.order.pizza['muzzarella'] = {
-    amount: 1, price: 2
-  };
-  order.order.pizza['calabresa'] = {
-    amout: 1,
-    price: 20
-  };
-
-  const pizzaUm = Object.keys(order.order.pizza)[0];
-  const pizzaDois = Object.keys(order.order.pizza)[1];
-  const drink = Object.values(order.order.drinks.coke)[0];
-  const totalPrice = order.payment['total'] = 50;
-console.log(`Olá ${buyer}, o total do seu pedido de ${pizzaUm}, ${pizzaDois} e ${drink} é R$${totalPrice},00.`)
-}
-
-orderModifier(order);
+console.log(addShift(lesson2, 'turno', 'manhã'))
