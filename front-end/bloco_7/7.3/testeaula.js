1b0d2baf357d40cbcf1b298fbbb7ee9977df9866
rodolfo_oliveira <rod.vgo@gmail.com>
2020-11-30 17:47:13 -0300

    organizar

--- front-end/bloco_7/7.3/testeaula.js
+++ front-end/bloco_7/7.3/testeaula.js
@@ -1,18 +1,32 @@
-const unitTest = require('assert');
+const unitTest = require("assert");
+const { error } = require("console");
 
 const sum = (number) => {
-  let summation = 0;
-  for(index = 1; index <= number; index += 1) {
-    summation += index;
+  if (typeof(number) !== 'number') {
+    throw Error("Valor deve ser número!");
+  } else {
+    let summation = 0;
+    for (index = 1; index <= number; index += 1) {
+      summation += index;
+    }
+    return summation;
   }
-  return summation
 };
 
-// console.log(sum(9));
 
 // typeof(sum) === 'function'
 
-unitTest.strictEqual(typeof(sum), 'function', 'O resultado esperado era function')
+unitTest.strictEqual(
+  typeof sum,
+  "function",
+  "O resultado esperado era function"
+);
 unitTest.strictEqual(sum(1), 1);
 unitTest.strictEqual(sum(3), 6);
 unitTest.strictEqual(sum(5), 15);
+unitTest.strictEqual(sum(0), 0);
+unitTest.throws(() => {
+  sum("3");
+});
+
+console.log(sum(5));
