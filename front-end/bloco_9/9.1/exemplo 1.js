26432e99864f4a3ceb9fc6a7f5be8ca30711f525
rodolfo_oliveira <rod.vgo@gmail.com>
2020-12-10 00:54:22 -0300

    bloco 8 e 9

---
+++ front-end/bloco_9/9.1/exemplo 1.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+console.log(numbers)
+
+setTimeout(() => assert.deepStrictEqual(numbers, [2, 3, 1]), 3000);
