abef3046bbc2fab1082dc54a9afb301a6ef9ea73
rodolfo_oliveira <rod.vgo@gmail.com>
2020-12-15 02:32:28 -0300

    refatoraçao 5

--- introduction-web_development/bloco_4/4.2/maiorValor.js
+++ introduction-web_development/bloco_4/4.2/maiorValor.js
@@ -1,11 +1,17 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [100, 9, 3, 19, 70, 8, 5, 2, 35, 27];
 
-let maiorValor = 0;
+let maiorValor = numbers[0];
 
-for(let i = 0; i < numbers.length; i++) {
+for(let i = 1; i < numbers.length; i++) {
     if(numbers[i] > maiorValor) {
         maiorValor = numbers[i]
     }
 }
 
 console.log(maiorValor);
+
+// Codigo refatorado
+
+const higherValue = (arr) => arr.reduce((acc, curr) => acc < curr? acc = curr: acc)
+
+console.log(higherValue(numbers))
