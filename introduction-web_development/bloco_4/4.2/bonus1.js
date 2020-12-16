c23831ec4b5173a61e62a52ff17a6568c1c01b91
rodolfo_oliveira <rod.vgo@gmail.com>
2020-12-16 11:58:50 -0300

    bonus 2

--- introduction-web_development/bloco_4/4.2/bonus1.js
+++ introduction-web_development/bloco_4/4.2/bonus1.js
@@ -1,21 +1,21 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let i = 1; i < numbers.length; i++) {
-  for (let j = 0; j < i; j++) {
-    if (numbers[i] < numbers[j]) {
-      let position = numbers[i];
+// for (let i = 1; i < numbers.length; i++) {
+//   for (let j = 0; j < i; j++) {
+//     if (numbers[i] < numbers[j]) {
+//       let position = numbers[i];
 
-      numbers[i] = numbers[j];
-      numbers[j] = position;
-    }
-  }
-}
-console.log(numbers);
+//       numbers[i] = numbers[j];
+//       numbers[j] = position;
+//     }
+//   }
+// }
+// console.log(numbers);
 
 
 // Codigo refatorado
 
 
-const sortNum = (arr) => arr.sort((a, b) => {a - b})
+const sortNum = (arr) => arr.sort((a, b) =>  a - b)
 
 console.log(sortNum(numbers))
