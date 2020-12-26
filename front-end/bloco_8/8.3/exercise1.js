3fa077040d889c6f97df8fd8ca66abe85aa8fb5f
rodolfo_oliveira <rod.vgo@gmail.com>
2020-12-26 00:03:30 -0300

    exercise 2

--- front-end/bloco_8/8.3/exercise1.js
+++ front-end/bloco_8/8.3/exercise1.js
@@ -9,7 +9,7 @@ const arrays = [
 
 
 function flatten() {
-  return arrays.reduce((acc, curr, index) => acc.concat(curr), [])
+  return arrays.reduce((acc, curr) => acc.concat(curr), [])
 }
 
 assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
