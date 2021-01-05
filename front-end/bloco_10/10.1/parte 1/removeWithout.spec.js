29486391ff0f652cf0eba2c6d0fcf551b002358e
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-04 23:42:32 -0300

    not equal array

--- front-end/bloco_10/10.1/parte 1/removeWithout.spec.js
+++ front-end/bloco_10/10.1/parte 1/removeWithout.spec.js
@@ -4,5 +4,7 @@ describe('myRemoveWithoutCopy', () => {
   it('returns an expected array', () => {
     expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
   });
-  
-})
+  it('returns a not equal array', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+});
