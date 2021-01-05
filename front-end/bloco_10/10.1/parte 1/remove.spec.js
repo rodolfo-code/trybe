5d38e6eed97a8d088e866e50401cd248c6ddd6c2
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-04 22:08:24 -0300

    returns an array other than

--- front-end/bloco_10/10.1/parte 1/remove.spec.js
+++ front-end/bloco_10/10.1/parte 1/remove.spec.js
@@ -4,4 +4,7 @@ describe('myRemove', () => {
   it('returns a new array without the item if the item belongs to it', () => {
     expect(myRemove([1, 2, 3, 4], 3)).not.toContain(3)
   })
+  it('returns an array other than [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
 });
