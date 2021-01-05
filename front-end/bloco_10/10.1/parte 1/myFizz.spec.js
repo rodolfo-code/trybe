0d6f1646aea002a305a5ee33d9480a210e2507d2
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-05 00:21:45 -0300

    divisible by 3 only

--- front-end/bloco_10/10.1/parte 1/myFizz.spec.js
+++ front-end/bloco_10/10.1/parte 1/myFizz.spec.js
@@ -4,5 +4,7 @@ describe('myFizzBuzz', () => {
   it('return "fizzBuzz" if the argument is divisible by 3 and 5', () => {
     expect('fizzbuzz').toBe(myFizzBuzz(15));
   });
-  
+  it(`returns 'fizz' if the argument is only divisible by 3`, () => {
+    expect('fizz').toBe(myFizzBuzz(9));
+  });
 });
