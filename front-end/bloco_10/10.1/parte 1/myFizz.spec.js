aa096db8bf782975aade0725bfde8e701b6acb93
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-05 00:16:20 -0300

    fizzbuzz test

--- front-end/bloco_10/10.1/parte 1/myFizz.spec.js
+++ front-end/bloco_10/10.1/parte 1/myFizz.spec.js
@@ -1,5 +1,8 @@
 const myFizzBuzz = require('./myFizz');
 
-describe('', () => {
+describe('myFizzBuzz', () => {
+  it('return "fizzBuzz" if the argument is divisible by 3 and 5', () => {
+    expect('fizzbuzz').toBe(myFizzBuzz(15));
+  });
   
-})
+});
