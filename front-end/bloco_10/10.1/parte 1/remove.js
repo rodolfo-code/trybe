b11ac12e028f81e5a78eee0ab60c33ab954e7497
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-04 22:17:11 -0300

    refact exercise

--- front-end/bloco_10/10.1/parte 1/remove.js
+++ front-end/bloco_10/10.1/parte 1/remove.js
@@ -1,12 +1,3 @@
-
-function myRemove(arr, item) {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i += 1) {
-    if (item !== arr[i]) {
-      newArr.push(arr[i]);
-    }
-  }
-  return newArr;
-}
+const myRemove = (arr, item) => arr.filter((element) => element !== item);
 
 module.exports = myRemove;
