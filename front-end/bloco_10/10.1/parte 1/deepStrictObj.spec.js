2e1d1700a322836a5a9f01beb4eff3240abc5e00
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-05 00:55:29 -0300

    comparing objects

--- front-end/bloco_10/10.1/parte 1/deepStrictObj.spec.js
+++ front-end/bloco_10/10.1/parte 1/deepStrictObj.spec.js
@@ -4,5 +4,7 @@ describe('arrayOfObjects', () => {
   it('comparing objects', () => {
   expect(arrayOfObjects[0]).toEqual(arrayOfObjects[1]);
   });
-  
+  it('comparing objects', () => {
+    expect(arrayOfObjects[0]).not.toEqual(arrayOfObjects[2]);
+  });
 });
