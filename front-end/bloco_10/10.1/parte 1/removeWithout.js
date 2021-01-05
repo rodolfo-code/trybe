702f62ff4fbde852081fb6484c2b5085066c0974
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-04 23:37:44 -0300

    returns as expected array

---
+++ front-end/bloco_10/10.1/parte 1/removeWithout.js
@@ -0,0 +1,24 @@
+const myRemove = require("./remove");
+
+// function myRemoveWithoutCopy(arr, item) {
+//   for (let i = 0, len = arr.length; i < len; i += 1) {
+//     if (arr[i] === item) {
+//       arr.splice(i, 1);
+//       i -= 1;
+//       len -= 1;
+//     }
+//   }
+
+//   return arr;
+// }
+
+const myRemoveWithoutCopy = (arr, item) => {
+  arr.forEach((element, index) => {
+   return element === item ? arr.splice(index, 1) : arr
+  });
+  return arr
+}
+
+
+console.log(myRemoveWithoutCopy([1, 2, 3, 4, 5], 3));
+module.exports = myRemoveWithoutCopy;
