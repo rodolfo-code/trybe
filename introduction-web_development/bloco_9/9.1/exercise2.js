b8708d7e7d211f7ed7f81ef81e7829154d8765c5
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-06 02:53:03 -0300

    exercise 2

--- introduction-web_development/bloco_9/9.1/exercise2.js
+++ introduction-web_development/bloco_9/9.1/exercise2.js
@@ -1,5 +1,5 @@
 const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
-  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+  `${name} is ${value} ${measurementUnit} apart from the Sun`
 
 const mars = {
   name: "Mars",
@@ -13,7 +13,7 @@ const venus = {
   name: "Venus",
   distanceFromSun: {
     value: 108200000,
-    measurementUnit: "kilometers",
+    measurementUnit: "kilometers"
   },
 };
 
@@ -21,10 +21,10 @@ const jupiter = {
   name: "Jupiter",
   distanceFromSun: {
     value: 778500000,
-    measurementUnit: "kilometers",
+    measurementUnit: "kilometers"
   },
 };
 
 console.log(planetDistanceFromSun(mars)); // A
-console.log(planetDistanceFromSun(venus)); // B
-console.log(planetDistanceFromSun(jupiter)); // C
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
