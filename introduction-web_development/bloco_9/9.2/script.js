565db7df241a721843d413d1949b9d97c5d905d7
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-07 17:50:03 -0300

    api pokemon

---
+++ introduction-web_development/bloco_9/9.2/script.js
@@ -0,0 +1,26 @@
+const appendPokemon = (pokemon) => {
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  li.innerHTML = pokemon;
+  ul.appendChild(li);
+}
+
+const appendNamePokemon = (namePokemon) => {
+  let h1 = document.querySelector('h1');
+  h1.innerHTML = namePokemon;
+}
+
+const fetchPokemon = () => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/charizard`)
+    .then((response) => {
+      response.json().then((data) => {
+        appendNamePokemon(data.name)
+        data.moves.map((value) => {
+          appendPokemon(value.move.name)
+        })
+      })
+    })  
+
+}
+
+window.onload = fetchPokemon();
