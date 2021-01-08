fc3207ddd6a314447bed52e30cec6a58e8baae10
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-07 21:57:35 -0300

    exercicio 1 completo

--- introduction-web_development/bloco_9/9.2/exercicios/script.js
+++ introduction-web_development/bloco_9/9.2/exercicios/script.js
@@ -1,10 +1,15 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
+const appendJoke = (joke) => {
+  let h2 = document.querySelector('h2');
+  h2.innerHTML = joke
+}
+
 const fetchJoke = () => {
   // Adicionar lógica aqui!
   fetch(API_URL, myObject)
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => appendJoke(data.joke))
   
 };
 
