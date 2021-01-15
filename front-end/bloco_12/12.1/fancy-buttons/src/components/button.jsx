65b8faf79c174c1506cc574b8f6a1b796d92294d
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-15 18:58:16 -0300

    conteudo do dia

---
+++ front-end/bloco_12/12.1/fancy-buttons/src/components/button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ManyButtons from './manyButtons';
+
+
+class RenderButton extends React.Component {
+  render() {
+    return (
+      <ManyButtons />
+    )
+  }
+}
+
+export default RenderButton;
