fb150f63b3e121eacae9dd2edfe7c8495b88b268
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-28 13:22:36 -0300

    ajustes

--- front-end/bloco_12/12.1/exercise-pokedex-state/src/Pokedex.js
+++ front-end/bloco_12/12.1/exercise-pokedex-state/src/Pokedex.js
@@ -1,64 +1,69 @@
-import React from 'react';
+import React from "react";
 
-import Pokemon from './Pokemon';
-import Button from './Button';
+import Pokemon from "./Pokemon";
+import Button from "./Button";
 
 class Pokedex extends React.Component {
-	constructor(props) {
-		super(props);
-		const { pokemons } = this.props;
-		
-		this.state = {
-			pokemons: pokemons,
-			index: 0
-		}
+  constructor(props) {
+    super(props);
+    const { pokemons } = this.props;
 
-		this.types = pokemons.map(pokemon => pokemon.type)
-			.reduce((acc, cur) => acc.includes(cur) ? acc : [...acc, cur], []);
+    this.state = {
+      pokemons: pokemons,
+      index: 0,
+    };
 
-		this.handleNext = this.handleNext.bind(this);
-		this.handleType = this.handleType.bind(this);
-	}
+    this.types = pokemons
+      .map((pokemon) => pokemon.type)
+      .reduce((acc, cur) => (acc.includes(cur) ? acc : [...acc, cur]), []);
 
-	handleNext() {
-		this.setState(previousState => {
-			const { index, pokemons } = previousState;
-			return { index: (index + 1) % pokemons.length }
-		});
-	}
+    this.handleNext = this.handleNext.bind(this);
+    this.handleType = this.handleType.bind(this);
+  }
 
-	handleType(event) {
-		const { target } = event;
-		const { textContent: filter } = target;
-		const { pokemons } = this.props;
-		this.setState({ 
-			pokemons: pokemons.filter(pokemon => filter === 'All' || pokemon.type === filter),
-			index: 0
-		});
-	}
+  handleNext() {
+    this.setState((previousState) => {
+      const { index, pokemons } = previousState;
+      return { index: (index + 1) % pokemons.length };
+    });
+  }
 
-	render() {
-		const { index, pokemons } = this.state;
-		
-		return (
-			<>
-				<div className="pokedex">
-					{ <Pokemon key={pokemons[index].id} pokemon={pokemons[index]} /> }	
-				</div>
+  handleType(event) {
+    const { target } = event;
+    const { textContent: filter } = target;
+    const { pokemons } = this.props;
+    this.setState({
+      pokemons: pokemons.filter(
+        (pokemon) => filter === "All" || pokemon.type === filter
+      ),
+      index: 0,
+    });
+  }
 
-				<Button
-					onClick={ this.handleNext }
-					disabled={ pokemons.length < 2 }
-				>
-					Next
-				</Button>
+  render() {
+    const { index, pokemons } = this.state;
 
-				<Button key="all" onClick={ this.handleType }>All</Button>
-				{ this.types.map(type => 
-					<Button key={ type } onClick={ this.handleType }>{ type }</Button> )}
-			</>
-		);
-	}
+    return (
+      <>
+        <div className="pokedex">
+          {<Pokemon key={pokemons[index].id} pokemon={pokemons[index]} />}
+        </div>
+
+        <Button onClick={this.handleNext} disabled={pokemons.length < 2}>
+          NEXT
+        </Button>
+
+        <Button key="all" onClick={this.handleType}>
+          All
+        </Button>
+        {this.types.map((type) => (
+          <Button key={type} onClick={this.handleType}>
+            {type}
+          </Button>
+        ))}
+      </>
+    );
+  }
 }
 
 export default Pokedex;
