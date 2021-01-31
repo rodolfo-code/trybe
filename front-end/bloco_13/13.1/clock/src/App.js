8468d4c6646a2d660755fbe28883ae11784b5cf2
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-31 17:49:42 -0300

    exercicio finalizado

---
+++ front-end/bloco_13/13.1/clock/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import Clock from './components/Clock';
+import Button from '@material-ui/core/Button'
+
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor() {
+    super();
+  
+    this.turnOffButton = this.turnOffButton.bind(this)
+    
+    this.state = {
+       showTime: true,
+    };
+  }
+
+  turnOffButton() {
+    const { showTime } = this.state
+    this.setState({
+      showTime: !showTime,
+    })
+  }  
+
+  render() {
+    const { showTime } = this.state
+    return (
+      <div className="App">
+        <header className="App-header">
+          {showTime ? 
+            <Clock turnOffButton={ this.turnOffButton } showTime={ showTime }/>
+            : <span>Clique para ver a hora</span>
+          }
+          <div className='button-class'>
+          
+            <Button variant="contained" color="primary" type='button' onClick={ this.turnOffButton }>Clique</Button>
+          </div>
+        </header>
+    </div>
+    )
+  }
+}
+
+export default App;
