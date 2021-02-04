3b2f297a0c31acea32b19d071319894b9ee398fa
rodolfo_oliveira <rod.vgo@gmail.com>
2021-02-04 13:56:55 -0300

    ajuste

--- front-end/bloco_13/13.2/trybe-clone/src/pages/Solutions.jsx
+++ front-end/bloco_13/13.2/trybe-clone/src/pages/Solutions.jsx
@@ -1,20 +1,12 @@
 import React, { Component } from 'react'
 // import PropTypes from 'prop-types'
-// import { Link } from 'react-router-dom';
 import RenderSolutionsList from '../components/RenderSolutionsList';
 
 class Solutions extends Component {
   render() {
     const { availableSolutions } = this.props;
-    console.log(this.props.availableSolutions[0])
     return (
       <section>
-        {/* <h3>Gabarito do módulo  </h3>
-        <ul>
-          { solutions12.map((day) =>
-            (<li key={ day }> <Link to={ `/solutions/${day}` }>{ day }</Link></li>
-          ))}
-        </ul> */}
         { availableSolutions.map((module) => <RenderSolutionsList solutions={ module }/>) }
       </section>
       
