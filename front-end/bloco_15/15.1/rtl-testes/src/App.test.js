4accc8e262f966211e4313ae3f8a72c8a1ac615b
rodolfo_oliveira <rod.vgo@gmail.com>
2021-02-22 13:15:31 -0300

    teste de botoes getAllByRole

--- front-end/bloco_15/15.1/rtl-testes/src/App.test.js
+++ front-end/bloco_15/15.1/rtl-testes/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Verifica que há campo input de email na tela', () => {
+test('1 - Verifica que há campo input de email na tela', () => {
   // Acessar elementos da sua tela
   const { getByLabelText } = render(<App />);
   const inputEmail = getByLabelText('Email');
@@ -9,9 +9,12 @@ test('Verifica que há campo input de email na tela', () => {
   // Fazer seu teste
   expect(inputEmail).toBeInTheDocument();
   expect(inputEmail.type).toBe('email')
-  
-
-  // Interagir com eles (se houver necessidade)
-
-
 });
+
+test('2 - Verifica se há botões', () => {
+  const { getAllByRole } = render(<App />);
+  const button = getAllByRole('button')
+
+  // Fazer os testes
+  expect(button.length).toBe(2)
+})
