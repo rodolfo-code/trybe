5717639c10a43a2185762574c6f0669b5bec1135
rodolfo_oliveira <rod.vgo@gmail.com>
2021-02-22 12:55:12 -0300

    primeiro teste

--- front-end/bloco_15/15.1/rtl-testes/src/App.test.js
+++ front-end/bloco_15/15.1/rtl-testes/src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Verifica que há campo input de email na tela', () => {
+  // Acessar elementos da sua tela
+  const { getByLabelText } = render(<App />);
+  const inputEmail = getByLabelText('Email');
+
+  // Fazer seu teste
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail.type).toBe('email')
+  
+
+  // Interagir com eles (se houver necessidade)
+
+
 });
