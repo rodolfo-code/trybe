3c6039694eec1ecfc84b856b6298c44b1aee73f1
rodolfo_oliveira <rod.vgo@gmail.com>
2021-02-22 22:01:51 -0300

    fireEvent

--- front-end/bloco_15/15.1/rtl-testes/src/App.test.js
+++ front-end/bloco_15/15.1/rtl-testes/src/App.test.js
@@ -1,28 +1,45 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('1 - Verifica que há campo input de email na tela', () => {
-  // Acessar elementos da sua tela
-  const { getByLabelText } = render(<App />);
-  const inputEmail = getByLabelText('Email');
+describe('Tela de inserção de email', () =>{
+  it('Verifica que há campo input de email na tela', () => {
+    // Acessar elementos da sua tela
+    const { getByLabelText } = render(<App />);
+    const inputEmail = getByLabelText('Email');
+  
+    // Fazer seu teste
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputEmail.type).toBe('email')
+  });
+  
+  it('Verifica se há botões', () => {
+    const { getAllByRole } = render(<App />);
+    const button = getAllByRole('button')
+  
+    // Fazer os testes
+    expect(button.length).toBe(2)
+  })
+  
+  it('Verifica se há botão com id específico', () => {
+    const { getByTestId } = render(<App/>);
+    const button = getByTestId('id-send')
+  
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveValue('Salvar')
+  })
 
-  // Fazer seu teste
-  expect(inputEmail).toBeInTheDocument();
-  expect(inputEmail.type).toBe('email')
-});
-
-test('2 - Verifica se há botões', () => {
-  const { getAllByRole } = render(<App />);
-  const button = getAllByRole('button')
-
-  // Fazer os testes
-  expect(button.length).toBe(2)
+  it('Verifica que ao inserir email e clicar Enviar o email aparece na tela', () => {
+    // Acessar elemento
+    const { getByTestId, getByLabelText } = render(<App />);
+    const emailInput = getByLabelText('Email');
+    const sendButton = getByTestId('id-send');
+    const userEmail = getByTestId('id-email-user');
+    // Interagir
+    fireEvent.change(emailInput, { target: { value: 'rod.vgo@gmail.com' } });
+    fireEvent.click(sendButton);
+    // Fazer o teste
+    expect(emailInput.value).toBe('');
+    expect(userEmail.textContent).toBe('Valor: rod.vgo@gmail.com');
+  })
 })
 
-test('3 - Verifica se há botão com id específico', () => {
-  const { getByTestId } = render(<App/>);
-  const button = getByTestId('id-send')
-
-  expect(button).toBeInTheDocument();
-  expect(button).toHaveValue('Salvar')
-})
