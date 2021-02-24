aa8f832e1d629549cfec792e0374d5b48357c1bb
rodolfo_oliveira <rod.vgo@gmail.com>
2021-02-24 00:56:55 -0300

    desafio 2

--- Live-Lectures/desafio-ninjas-js/desafio2/somatorio.js
+++ Live-Lectures/desafio-ninjas-js/desafio2/somatorio.js
@@ -1,1 +1,12 @@
 // Seu código aqui
+const lista = require('./lista.json');
+
+const somatorio = () => {
+  const prices = lista.map((object) => object.price.split('')
+    .filter((e) => (Number(e))).join(''))
+    .reduce((acc, cur) => acc + (parseFloat(cur, 10)), 0);
+
+  return prices;
+};
+
+console.log(somatorio());
