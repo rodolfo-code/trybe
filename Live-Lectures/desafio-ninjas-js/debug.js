59ef5ffa41664aacda25d6f11e4293d781a347fe
rodolfo_oliveira <rod.vgo@gmail.com>
2021-02-23 23:42:39 -0300

    iniciando desafio

---
+++ Live-Lectures/desafio-ninjas-js/debug.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+// fs.writeFile('mynewfile3.txt', 'This is my text', function (err) {
+//   if (err) throw err;
+//   console.log('Replaced!');
+// });
---
+++ Live-Lectures/desafio-ninjas-js/desafio3/ordenar.js
@@ -0,0 +1,3 @@
+const data = require('./data.json');
+
+// seu código aqui
