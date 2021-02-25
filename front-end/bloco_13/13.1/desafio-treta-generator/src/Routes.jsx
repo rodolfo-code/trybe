4b050e83ea5ed62ecd912578042e2c2d9d0c98e6
rodolfo_oliveira <rod.vgo@gmail.com>
2021-02-25 02:10:17 -0300

    criando rotas

--- front-end/bloco_13/13.1/desafio-treta-generator/src/Routes.jsx
+++ front-end/bloco_13/13.1/desafio-treta-generator/src/Routes.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import EscolhaAnimal from './components/EscolhaAnimal'
+import EscolhaCerto from './components/EscolhaCerto'
+import EscolhaPior from './components/EscolhaPior'
+import GerandoTreta from './components/GerandoTreta'
 import InputName from './components/InputName'
 
 function Routes() {
   return (
     <Switch>
       <Route path='/' render={InputName}/>
+      <Route path='/preferencia' render={EscolhaCerto} />
+      <Route path='/comida' render={EscolhaPior} />
+      <Route path='/animal' render={EscolhaAnimal} />
+      <Route path='/manchete' render={GerandoTreta} />
     </Switch>
   )
 }
