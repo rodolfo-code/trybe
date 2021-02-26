ac9387dd11a09ecea84aeda31dcbec7d123a6b20
rodolfo_oliveira <rod.vgo@gmail.com>
2021-02-26 18:11:33 -0300

    componente treta

--- front-end/bloco_13/13.1/desafio-treta-generator/src/pages/GerandoTreta.jsx
+++ front-end/bloco_13/13.1/desafio-treta-generator/src/pages/GerandoTreta.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import { Container } from 'reactstrap';
 
 function GerandoTreta(props) {
- const { name, bolacha, worst, animal } = props;
+  const { name, bolacha, worst, animal } = props;
+
+  const conditional = () => {
+    if(worst === 'SOPA') return `${worst} não é janta`
+    return `${worst} é ruim`
+  }
 
   return (
-    <div>
-      Muita treta vish
-      {name}
-      {bolacha}
-      {worst}
-      {animal}
-    </div>
+    <>
+      <h1><strong>GERANDO A TRETA</strong></h1>
+      <Container className="treta-container">
+        <p className="treta-text">
+          {`Em entrevista internacional, ${name} teve que debater sobre a discussão mais acirrada do século,
+          é biscoito ou bolacha? ${name} foi direto na canela dizendo que o certo é ${bolacha}!
+          E ainda complementou que ${conditional()} e que nem seu ${animal} come!`}
+        </p>
+      </Container>
+    </>
   )
 }
 
