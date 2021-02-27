d4302c40ff89bb863e8881bb0965fdec2db3e93d
rodolfo_oliveira <rod.vgo@gmail.com>
2021-02-27 13:57:56 -0300

    criaçao do reducer

---
+++ front-end/bloco_16/16.1/redux-init/script.js
@@ -0,0 +1,15 @@
+// Padrão da action
+// const action = {
+//   type: 'INCREMENT', /* obrigatorio para o action funcionar */
+//   payload: {
+//     amount: 1,
+//   }
+// }
+
+// Criação do reducer
+function counterReducer(state = 0, action) {
+
+}
+
+// Configurar store
+const store = Redux.createStore(counterReducer)
