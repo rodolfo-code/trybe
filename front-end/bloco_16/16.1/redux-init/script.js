492bafc8090544a5e15e916e1d296bdb007e07b9
rodolfo_oliveira <rod.vgo@gmail.com>
2021-02-27 14:43:07 -0300

    evento de incrementar

--- front-end/bloco_16/16.1/redux-init/script.js
+++ front-end/bloco_16/16.1/redux-init/script.js
@@ -8,8 +8,34 @@
 
 // Criação do reducer
 function counterReducer(state = 0, action) {
-
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+  
+    default:
+      return state
+  }
 }
 
 // Configurar store
 const store = Redux.createStore(counterReducer)
+
+// Adicionar o disparo das actions no click dos botões
+
+const buttonIncrement = document
+  .getElementById('increment')
+  .addEventListener('click', () => {
+    const incrementAction = {
+      type: 'INCREMENT'
+    };
+    store.dispatch(incrementAction)
+  })
+
+  // Ler dados do store global
+
+  store.subscribe(() => {
+    const valueSpan = document.getElementById('value');
+    valueSpan.innerHTML = store.getState()
+  })
+
+  //
