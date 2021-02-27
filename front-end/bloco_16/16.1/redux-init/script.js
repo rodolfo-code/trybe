83bc6c0569099ee00d809c1e5b57c7a78a9c2950
rodolfo_oliveira <rod.vgo@gmail.com>
2021-02-27 15:10:18 -0300

    funçao de decrementar e incrementar 10

--- front-end/bloco_16/16.1/redux-init/script.js
+++ front-end/bloco_16/16.1/redux-init/script.js
@@ -11,14 +11,20 @@ function counterReducer(state = 0, action) {
   switch (action.type) {
     case "INCREMENT":
       return state + 1;
-  
+    case "DECREMENT":
+      return state - 1
+    case "INCREMENT-10":
+      return state + 10
     default:
       return state
   }
 }
 
 // Configurar store
-const store = Redux.createStore(counterReducer)
+const store = Redux.createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 // Adicionar o disparo das actions no click dos botões
 
@@ -31,11 +37,27 @@ const buttonIncrement = document
     store.dispatch(incrementAction)
   })
 
-  // Ler dados do store global
-
-  store.subscribe(() => {
-    const valueSpan = document.getElementById('value');
-    valueSpan.innerHTML = store.getState()
+const buttonDecrement = document
+  .getElementById('decrement')
+  .addEventListener('click', () => {
+    const decrementAction = {
+      type: 'DECREMENT'
+    }
+    store.dispatch(decrementAction);
+  })
+    
+const buttonIncrement10 = document
+  .getElementById('increment-10')
+  .addEventListener('click', () => {
+    const increment10Action = {
+      type: 'INCREMENT-10'
+    }
+    store.dispatch(increment10Action)
   })
 
-  //
+  // Ler dados do store global
+
+store.subscribe(() => {
+  const valueSpan = document.getElementById('value');
+  valueSpan.innerHTML = store.getState()
+})
