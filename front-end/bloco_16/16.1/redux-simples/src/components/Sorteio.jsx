803e7d991753d14b3a6a662ae540c81e2b423537
rodolfo_oliveira <rod.vgo@gmail.com>
2021-03-01 10:36:56 -0300

    distribuindo props do state global

--- front-end/bloco_16/16.1/redux-simples/src/components/Sorteio.jsx
+++ front-end/bloco_16/16.1/redux-simples/src/components/Sorteio.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import Card from './Card'
+import React from 'react';
+import Card from './Card';
+import { connect } from 'react-redux';
 
 function Sorteio(props) {
+  const { min, max } = props;
+  const rand = parseInt(Math.random() * (max - min)) + min
+  console.log(rand)
   return (
     <Card title="Sorteio de um Número" purple>
       <div>
-        <spa>
+        <span>
           <span>Resultado:</span>
-          <strong>{10}</strong>
-        </spa>
+          <strong>{rand}</strong>
+        </span>
       </div>
     </Card>
   )
 }
 
-export default Sorteio;
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max
+  }
+}
+
+export default connect(mapStateToProps)(Sorteio);
