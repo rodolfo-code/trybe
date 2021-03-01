c64ab5f36d323d83e27f33d04a7fb1a19348ef95
rodolfo_oliveira <rod.vgo@gmail.com>
2021-03-01 06:44:21 -0300

    OO

--- introduction-web_development/bloco_7/7.2/teste.js
+++
@@ -1,8 +0,0 @@
-
-function contem(mat, not) {
-  const materia = [ 'materia', 'Matemática', 'noite' ];
-  let inclui = materia.includes(mat) && materia.includes(not)
-  return inclui;
-}
-
-console.log(contem('materia', 'noite'))
