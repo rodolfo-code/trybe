2e74a2ea301f1495627195f99920d3614f169747
Rodolfo Oliveira <68041162+rodolfo-code@users.noreply.github.com>
2021-02-28 23:11:32 -0300

    Update criacao_objetos.js

--- introduction-web_development/bloco_7/7.2/criacao_objetos.js
+++ introduction-web_development/bloco_7/7.2/criacao_objetos.js
@@ -1,1 +1,18 @@
 // notaçao literal
+const obj1 = {}
+console.log(obj1)
+
+//Object em js
+const obj2 = new Object;
+console.log(obj2)
+
+// funçao construtora
+function Produto(nome, preco, desc) {
+  this.nome = nome
+  this.getPrecoComDesconto = () => {
+    return preco * (1 - desc)
+  }
+}
+const p1 = new Produto('Caneta', 7.99, 0.15)
+const p2 = new Produto('Norebook', 2998.99, 0.25)
+console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto())
