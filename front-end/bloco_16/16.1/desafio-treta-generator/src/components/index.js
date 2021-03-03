b8515219943ccc7e17bdaf85e027bdf305dfb59c
rodolfo_oliveira <rod.vgo@gmail.com>
2021-03-03 03:32:32 -0300

    ajustes

--- front-end/bloco_16/16.1/desafio-treta-generator/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './Button/Button';
-export { default as Radio } from './Radio/Radio';
-export { default as Input } from './Input/Input';
-export { default as Select } from './Select/Select';
