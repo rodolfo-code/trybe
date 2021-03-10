ef2fd92f4bbf3dc5e61539a6b58c9b812a4afda7
rodolfo_oliveira <rod.vgo@gmail.com>
2021-03-09 23:17:56 -0300

    3

--- introduction-web_development/exercicios/exercicios.js
+++ introduction-web_development/exercicios/exercicios.js
@@ -1,8 +1,39 @@
-// Qual o mes?
-
-const obj = {
-  1: 'janeiro',
-  2: 'fevereiro',
-  3: 'março',
-  4
+// Retornando mes
+const obj = [
+  'janeiro',
+  'fevereiro',
+  'março',
+  'abril',
+  'maio',
+  'junho', 
+  'julho',
+  'agosto',
+  'setembro',
+  'outubro',
+  'novembro',
+  'dezembro'
+]
+const mes = (n) => {
+  if(n == 0 || n > 12) return 'Esse mes nao existe cara'
+  return obj[--n]
 }
+console.log(mes(13))
+
+// Um mair q outro
+
+const higher = (x, y) => {
+  return (x === y) && true
+}
+console.log(higher(5, 0))
+
+// Revert
+
+const revert = (param) => {
+  if(typeof param === 'boolean') return !param
+  return param * (-1)
+
+}
+
+console.log(revert(true))
+
+//
