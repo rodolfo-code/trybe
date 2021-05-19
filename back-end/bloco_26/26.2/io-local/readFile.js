d7cb681f2be926a703bfbeb830cf143ea19c58b5
rodolfo_oliveira <rod.vgo@gmail.com>
2021-05-19 09:31:39 -0300

    read/write

---
+++ back-end/bloco_26/26.2/io-local/readFile.js
@@ -0,0 +1,16 @@
+const fs = require("fs/promises");
+
+const nomeDoArquivo = "meu-arquivo.txt";
+
+fs.readFile(nomeDoArquivo, "utf8")
+  .then((data) => {
+    console.log(`Conteúdo do arquivo: ${data}`);
+  })
+  .catch((err) => {
+    console.error(
+      `Não foi possível ler o arquivo ${nomeDoArquivo}\n Erro: ${err}`,
+    );
+    process.exit(1);
+  });
+
+console.log("eeee");
