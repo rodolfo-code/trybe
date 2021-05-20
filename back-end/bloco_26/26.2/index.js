80198b318d3f0117d6629e30ac6f2371d1d2af4a
rodolfo_oliveira <rod.vgo@gmail.com>
2021-05-19 23:29:49 -0300

    exercicio 1-2

---
+++ back-end/bloco_26/26.2/index.js
@@ -0,0 +1,42 @@
+function recebeParans(param1, param2, param3) {
+  const promise = new Promise((resolve, reject) => {
+    if ([param1, param2, param3].some((param) => typeof param !== "number")) {
+      return reject(new Error(`Informe apenas números`));
+    }
+
+    const op = (param1 + param2) * param3;
+
+    if (op < 50) {
+      return reject(new Error("Valor muito baixo"));
+    }
+
+    resolve(op);
+  });
+
+  return promise;
+}
+
+function rand() {
+  return Math.ceil(Math.random() * 100);
+}
+
+const randomArray = Array.from({ length: 3 }, () => rand());
+
+function call() {
+  recebeParans(...randomArray)
+    .then((result) => console.log(result))
+    .catch((err) => console.error(err.message));
+}
+
+call();
+
+async function call2() {
+  try {
+    const result = await recebeParans(...randomArray);
+    console.log(result);
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+call2();
