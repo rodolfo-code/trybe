7a00b7e383205738f5f9df1c0c49f5b3c9c4f97a
rodolfo_oliveira <rod.vgo@gmail.com>
2021-05-30 01:28:57 -0300

    org

--- back-end/bloco_26/26.4/hello-express/index.js
+++ back-end/bloco_26/26.4/hello-express/index.js
@@ -1,13 +1,32 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
 
-app.get('/hello', handleHelloWorldRequest);
+app.use(bodyParser.json());
+
+app.get('/', (_req, res) => {
+  res.send('Hello world');
+});
+
+const langs = ['HTML', 'CSS', 'Javascript', 'React'];
+
+app.get('/langs', (_req, res) => {
+  res.send(langs);
+});
+
+app.post('/langs', (req, res) => {
+  const { name } = req.body;
+  langs.push(`${name}`);
+  res.send(`Linguagem ${name} salva`);
+});
+
+app.delete('/langs', (req, res) => {
+  const { name } = req.body;
+  langs.splice(langs.indexOf(name), 1);
+  res.send(`Linguagem ${name} deletada`);
+});
 
 app.listen(3000, () => {
   console.log('Aplicação ouvindo a porta 3000');
 });
-
-function handleHelloWorldRequest(req, res) {
-  res.status(200).send('Hello world');
-}
