61ddaacbb898aad2bcee85d0e54a52e0094fb220
rodolfo_oliveira <rod.vgo@gmail.com>
2021-06-29 17:09:37 -0300

    encontrar autor por id

--- back-end/bloco_27/27.1/conexao-mysql/index.js
+++ back-end/bloco_27/27.1/conexao-mysql/index.js
@@ -6,13 +6,25 @@ const Books = require('./models/Books');
 const app = express();
 const PORT = 3000;
 
-app.get('/authors', async (_req, res) => {
+app.get('/authors', async (req, res) => {
   const authors = await Author.getAll();
 
   res.status(200).json(authors);
 });
 
-app.get('/books', async (_req, res) => {
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const author = await Author.findById(id);
+
+  if (!author) {
+    return res.status(404).json({ message: 'NOT FOUND' });
+  }
+
+  return res.status(200).json(author);
+});
+
+app.get('/books', async (req, res) => {
   const books = await Books.getAllBooks();
 
   res.status(200).json(books);
--- back-end/bloco_27/27.1/conexao-mysql/models/Author.js
+++ back-end/bloco_27/27.1/conexao-mysql/models/Author.js
@@ -31,6 +31,25 @@ async function getAll() {
   return authors.map(serialize).map(getNewAuthor);
 }
 
+async function findById(id) {
+  const [authorData] = await connection.execute(
+    'SELECT id, first_name, middle_name, last_name FROM authors WHERE id=?',
+    [id],
+  );
+
+  if (authorData.length === 0) return null;
+
+  const { firstName, middleName, lastName } = authorData.map(serialize)[0];
+
+  return getNewAuthor({
+    id,
+    firstName,
+    middleName,
+    lastName,
+  });
+}
+
 module.exports = {
   getAll,
+  findById,
 };
