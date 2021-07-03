ca9f82aa8d28c2fadf2b3749be92d3fc3f61fa4f
rodolfo_oliveira <rod.vgo@gmail.com>
2021-07-03 07:47:20 -0300

    metodo get by authorId mongodb

--- back-end/bloco_27/27.1/conexao-mongodb/src/index.js
+++ back-end/bloco_27/27.1/conexao-mongodb/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const authors = require('./routes/routeAuthors');
+const books = require('./routes/routeBooks');
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,7 @@ app.use(express.json());
 const PORT = 3000;
 
 app.use('/authors', authors);
+app.use('/books', books);
 
 app.listen(PORT, () => {
   console.log(`App listen on port ${PORT}`);
