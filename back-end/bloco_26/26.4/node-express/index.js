65de88692540ede8982e901b4cbbf14b335b2054
rodolfo_oliveira <rod.vgo@gmail.com>
2021-09-01 19:40:20 -0300

    put, delete

--- back-end/bloco_26/26.4/node-express/index.js
+++ back-end/bloco_26/26.4/node-express/index.js
@@ -4,12 +4,6 @@ const Router = require('./router');
 const app = express();
 const PORT = 3001;
 
-// const books = [
-//   { id: 1, title: 'Senhor dos aneis', author: 'J.R.R Tokien' },
-//   { id: 2, title: 'Dune', author: 'Frank Herbert' },
-//   { id: 3, title: 'Foundation', author: 'Isaac Asimov' },
-// ];
-
 app.use(express.json());
 
 app.use('/books', Router.books);
--- back-end/bloco_26/26.4/node-express/router/routerBooks.js
+++ back-end/bloco_26/26.4/node-express/router/routerBooks.js
@@ -5,6 +5,14 @@ const auxFunction = require('../utils');
 const DIRETORIO = './books.json';
 
 const router = express.Router();
+router.get('/search', async (req, res) => {
+  const { title } = req.query;
+
+  const books = await auxFunction();
+  const search = books.filter((item) => item.title === title);
+
+  res.status(200).json({ books: search });
+});
 
 router.get('/', async (_req, res) => {
   const books = await auxFunction();
@@ -12,14 +20,23 @@ router.get('/', async (_req, res) => {
   return res.status(200).json({ books: books });
 });
 
-router.post('/', async (req, res) => {
-  const newBook = req.body;
-  console.log(req.body);
-
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
   const books = await auxFunction();
 
+  const bookById = books.find((book) => book.id === +id);
+  if (!bookById) {
+    return res.status(404).json({ message: 'Book not found' });
+  }
+
+  return res.status(200).json({ message: bookById });
+});
+
+router.post('/', async (req, res) => {
+  const newBook = req.body;
+
+  const books = await auxFunction();
   books.push(newBook);
-  console.log(books);
 
   await fs.writeFile(DIRETORIO, JSON.stringify(books));
 
@@ -31,19 +48,18 @@ router.put('/:id', async (req, res) => {
   const { id } = req.params;
 
   const books = await auxFunction();
-  const index = books.findIndex((book) => book.id == id);
+  const index = books.findIndex((book) => book.id === +id);
 
   if (index == -1) {
     return res.status(400).json({ message: 'livro nao encontrado' });
   }
 
   const newBook = {
-    id: parseInt(id),
+    id: +id,
     ...alteredBook,
   };
 
   books.splice(index, 1, newBook);
-
   await fs.writeFile(DIRETORIO, JSON.stringify(books));
 
   return res.status(200).json(books);
@@ -55,15 +71,12 @@ router.delete('/:id', async (req, res) => {
   const books = await auxFunction();
 
   const index = books.findIndex((book) => book.id == id);
-  console.log(books);
-  console.log(index);
 
   if (index == -1) {
     return res.status(400).json({ message: 'livro nao encontrado' });
   }
 
   books.splice(index, 1);
-  console.log(books);
 
   await fs.writeFile(DIRETORIO, JSON.stringify(books));
 
