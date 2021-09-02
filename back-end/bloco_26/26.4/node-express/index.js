5936c18a9459f6db81d462694e34e0132e70d64b
rodolfo_oliveira <rod.vgo@gmail.com>
2021-09-02 18:22:16 -0300

    middlewares

--- back-end/bloco_26/26.4/node-express/index.js
+++ back-end/bloco_26/26.4/node-express/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+
 const Router = require('./router');
+const authToken = require('./middlewares/authToken');
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
-
-app.use('/books', Router.books);
+app.use(cors());
 
 app.get('/', (_req, res) => {
   res.json({ message: 'Hello world' });
 });
+
+app.use(authToken);
+
+app.use('/books', Router.books);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${3001}`);
