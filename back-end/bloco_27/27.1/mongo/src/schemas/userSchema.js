ee893491598293fc3b481485a53c7079e0d40ce2
rodolfo_oliveira <rod.vgo@gmail.com>
2021-09-09 23:32:54 -0300

    finalizando crud

--- back-end/bloco_27/27.1/mongo/src/schemas/userSchema.js
+++ back-end/bloco_27/27.1/mongo/src/schemas/userSchema.js
@@ -24,4 +24,19 @@ const userSchema = Joi.object({
   }),
 });
 
+// const userSchema = Joi.object({
+//   firstName: Joi.string().required(),
+//   lastName: Joi.string().required(),
+//   email: Joi.string().email().required(),
+//   password: Joi.string().min(6).required(),
+// }).messages({
+//   'any.required': 'O campo {
+// {: id="label" }} é obrigatório',
+//   'string.min': 'O campo {
+// {: id="label" }} deve ter, pelo menos, {
+// {: id="limit" }} caracteres',
+//   'string.email': 'Informe um email válido no campo {
+// {: id="label" }}',
+// });
+
 module.exports = userSchema;
